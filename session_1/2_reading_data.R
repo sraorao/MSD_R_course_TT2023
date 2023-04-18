2365d1b71692b989940203595ec27b2323279179
Srinivasa Rao Rao <sraorao@users.noreply.github.com>
2023-04-18 23:01:30 +0100

    date

--- session_1/2_reading_data.R
+++ session_1/2_reading_data.R
@@ -1,5 +1,5 @@
 ###R for biologists
-##Irina & Rao, 01/03/2023
+##Irina & Rao, 19/04/2023
 
 # https://www.nature.com/articles/sdata20179
 # Article title: Systematic high-content genome-wide RNAi screens of endothelial 
